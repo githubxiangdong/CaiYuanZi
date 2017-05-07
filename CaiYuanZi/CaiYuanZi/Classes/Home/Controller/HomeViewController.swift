9451fb8974fded7e1f72c521c645ca44d57b69c5
githubxiangdong <719326543@qq.com>
2017-05-08 00:01:37 +0800

    主界面cell搭建

--- CaiYuanZi/CaiYuanZi/Classes/Home/Controller/HomeViewController.swift
+++ CaiYuanZi/CaiYuanZi/Classes/Home/Controller/HomeViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
+private let kCellID = "kCellID"
 class HomeViewController: UIViewController {
 
     // MARK:- 懒加载属性
+    fileprivate lazy var tableView : UITableView = {
     
+        // 创建tableView
+        let tableView = UITableView(frame: self.view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "BusinessTableViewCell", bundle: nil), forCellReuseIdentifier: kCellID)
+        return tableView
+    }()
     
     // MARK:- 系统回调函数
     override func viewDidLoad() {
@@ -24,8 +33,43 @@ class HomeViewController: UIViewController {
 
 // MARK:- 设置ui界面
 extension HomeViewController {
+    
     fileprivate func setupUI() {
     
-        
+        // 1, 将table添加到view上
+        view.addSubview(tableView)
     }
 }
+
+//MARK:- 调用 tableView的数据源代理
+extension HomeViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCellID, for: indexPath)
+
+        return cell
+    }
+}
+
+//MARK:- 调用tbleView的代理
+extension HomeViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 210
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ CaiYuanZi/CaiYuanZi/Classes/Main/View/BusinessTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessTableViewCell.swift
+//  CaiYuanZi
+//
+//  Created by 张向东 on 2017/5/7.
+//  Copyright © 2017年 张向东. All rights reserved.
+//
+
+import UIKit
+
+class BusinessTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
