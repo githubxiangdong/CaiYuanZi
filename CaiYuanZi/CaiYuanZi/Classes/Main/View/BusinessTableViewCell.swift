4c1798a25af29e16061433100934a0eeab378205
githubxiangdong <719326543@qq.com>
2017-05-12 08:38:26 +0800

    添加控件属性

--- CaiYuanZi/CaiYuanZi/Classes/Main/View/BusinessTableViewCell.swift
+++ CaiYuanZi/CaiYuanZi/Classes/Main/View/BusinessTableViewCell.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class BusinessTableViewCell: UITableViewCell {
 
+    // MARK;- 控件属性
+    @IBOutlet weak var businessIcon: UIImageView!
+    @IBOutlet weak var businessTitle: UILabel!
+    @IBOutlet weak var transportType: UIImageView!
+    @IBOutlet weak var gard: UILabel!
+    @IBOutlet weak var sales: UILabel!
+    @IBOutlet weak var arriveTime: UILabel!
+    
+    @IBOutlet weak var minPrice: UILabel!
+    @IBOutlet weak var transportPrice: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    
+    @IBOutlet weak var newLabel: UILabel!
+    @IBOutlet weak var fullLabel: UILabel!
+    @IBOutlet weak var activities: UILabel!
+    @IBOutlet weak var state: UIButton!
+    
+    @IBOutlet weak var very1Icon: UIImageView!
+    @IBOutlet weak var very2Iocn: UIImageView!
+    @IBOutlet weak var very3Icon: UIImageView!
+    @IBOutlet weak var veryIcon: UIImageView!
+    @IBOutlet weak var very1Label: UILabel!
+    @IBOutlet weak var very2Label: UILabel!
+    @IBOutlet weak var very3Label: UILabel!
+    @IBOutlet weak var very4Label: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    
+    //MARK:- 活动多了后展开和收起活动类容
+    @IBAction func clickChange(_ sender: Any) {
+        
     }
     
 }
